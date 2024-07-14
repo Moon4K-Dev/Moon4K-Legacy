ace80f57fccd6935d07295203ebc2f8f16909388
Ty [Indev] <168133165+tyindev@users.noreply.github.com>
2024-07-14 16:20:05 -0400

    better pause

--- source/substates/PauseSubstate.hx
+++ source/substates/PauseSubstate.hx
@@ -1,70 +1,118 @@
 package substates;
 
+import substates.SwagSubState;
+import options.Controls;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.input.keyboard.FlxKey;
 import flixel.system.FlxSound;
-import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
-import mobile.flixel.FlxVirtualPad;
-import flixel.FlxSubState;
-import flixel.FlxCamera;
-import states.PlayState;
+import flixel.text.FlxText;
 
 class PauseSubstate extends SwagSubState
 {
-	#if mobile
-	var flxPad:FlxVirtualPad;
-	var controlsCam:FlxCamera;
-	#end	
+    var grpMenuShit:FlxTypedGroup<FlxText>;
 
-	public function new()
-	{
-		super();
+    var menuItems:Array<String> = ['Resume', 'Restart Song', 'Options', 'Exit to menu'];
+    var curSelected:Int = 0;
+
+    public function new()
+    {
+        super();
 
         var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+        bg.alpha = 0.6;
         bg.scrollFactor.set();
-        bg.alpha = 0.65;
-		bg.screenCenter();
         add(bg);
 
-		var songInfo:FlxText = new FlxText(20, 15, 0, "", 32);
-        songInfo.text += "Song: " + PlayState.curSong;
-		songInfo.scrollFactor.set();
-		songInfo.setFormat(Paths.font("vcr.ttf"), 32);
-		songInfo.updateHitbox();
-		add(songInfo);
-	}
+        var songInfo:FlxText = new FlxText(20, 15, 0, "", 32);
+        songInfo.text += "Song: " + states.PlayState.curSong;
+        songInfo.scrollFactor.set();
+        songInfo.setFormat(Paths.font("vcr.ttf"), 32);
+        songInfo.updateHitbox();
+        add(songInfo);
 
-	override function update(elapsed:Float)
-	{
-		super.update(elapsed);
-        #if mobile
-		flxPad = new FlxVirtualPad(NONE, A);
-		add(flxPad);
-		#end
+        grpMenuShit = new FlxTypedGroup<FlxText>();
+        add(grpMenuShit);
 
-        #if (!mobile)
-		if (FlxG.keys.justPressed.ENTER)
-		{
-            close();
-		}
-        #end
-
-        #if mobile
-        if (flxPad.buttonA.pressed)
+        for (i in 0...menuItems.length)
         {
-            close();
+            var swaggerTXT:FlxText = new FlxText(0, 0, FlxG.width, menuItems[i]);
+            swaggerTXT.setFormat(Paths.font('vcr.ttf'), 24, FlxColor.WHITE, "center");
+            swaggerTXT.alpha = 0.6; // Default alpha
+            grpMenuShit.add(swaggerTXT);
         }
-        #end
-	}
 
-	override function destroy()
-	{
-		super.destroy();
-	}	
+        centerMenuItems();
+
+        cameras = [FlxG.cameras.list[1]];
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        var upP = Controls.UI_UP;
+        var downP = Controls.UI_DOWN;
+        var accepted = Controls.ACCEPT;
+
+        if (upP)
+        {
+            changeSelection(-1);
+        }
+        if (downP)
+        {
+            changeSelection(1);
+        }
+
+        if (accepted)
+        {
+            var daSelected:String = menuItems[curSelected];
+
+            switch (daSelected)
+            {
+                case "Resume":
+                    close();
+                case "Restart Song":
+                    FlxG.resetState();
+				case "Options":
+					//transitionState(new states.OptionSelectState());
+                case "Exit to menu":
+                    transitionState(new states.Freeplay());
+            }
+        }
+    }
+
+    override function destroy()
+    {
+        super.destroy();
+    }
+
+    function changeSelection(change:Int = 0):Void
+    {
+        curSelected += change;
+
+        if (curSelected < 0)
+            curSelected = menuItems.length - 1;
+        if (curSelected >= menuItems.length)
+            curSelected = 0;
+
+        centerMenuItems();
+    }
+
+    function centerMenuItems():Void
+    {
+        var totalHeight:Float = 70 * menuItems.length;
+        var startY:Float = (FlxG.height - totalHeight) / 2;
+
+        for (i in 0...grpMenuShit.members.length)
+        {
+            var item = grpMenuShit.members[i];
+            item.y = startY + i * 70;
+
+            item.alpha = (i == curSelected) ? 1 : 0.6;
+        }
+    }
 }
