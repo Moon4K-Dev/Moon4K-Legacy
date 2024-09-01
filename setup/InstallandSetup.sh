d97fc5669ef1e72f5a9209aad9711374e2b60d4b
Phlox <168133165+yophlox@users.noreply.github.com>
2024-09-01 01:12:34 -0400

    stuff

--- setup/InstallandSetup.sh
+++ setup/InstallandSetup.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install haxe -y
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Haxe installed and properly set up!
-echo Installing needed libs for Lantern Framework!
+echo Installing needed libs for Moon4K!
 haxelib install lime 7.9.0
 haxelib install openfl
 haxelib --never install flixel 4.11.0
@@ -13,11 +13,15 @@ haxelib run lime setup -y
 haxelib install flixel-tools
 haxelib install flixel-addons 2.11.0
 haxelib install flixel-ui
-haxelib git polymod https://github.com/larsiusprime/polymod
-haxelib git hscript-improved https://github.com/YoshiCrafter29/hscript-improved
+haxelib git hscript-improved https://github.com/FNF-CNE-Devs/hscript-improved
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
+haxelib install hxCodec
+haxelib install newgrounds 1.1.4
+haxelib git tentools https://github.com/TentaRJ/tentools
+haxelib git hxcpp https://github.com/yophlox/fxcpp
 haxelib set lime 7.9.0
 haxelib set flixel 4.11.0
 haxelib set flixel-addons 2.11.0
 echo Setting up lime..
 haxelib run lime setup -y
-echo Lantern Framework! fully set up for building!
+echo Moon4K! fully set up for building!
--- setup/InstallandSetupLibs.bat
+++ setup/InstallandSetupLibs.bat
@@ -1,9 +1,9 @@
 @echo off
-title Lantern Framework! Setup - Start
+title Moon4K! Setup - Start
 echo Make sure Haxe and HaxeFlixel is installed (4.1.5 is important)!
 echo Press any key to install required libraries.
 pause >nul
-title Lantern Framework! Setup - Installing libraries
+title Moon4K! Setup - Installing libraries
 echo Installing haxelib libraries...
 cinst haxe --version 4.1.5 -y
 mkdir "%HAXELIB_ROOT%"
@@ -12,13 +12,17 @@ haxelib install lime 7.9.0
 haxelib install openfl
 haxelib --never install flixel 4.11.0
 haxelib run lime setup flixel
-haxelib git polymod https://github.com/larsiusprime/polymod
-haxelib git hscript-improved https://github.com/YoshiCrafter29/hscript-improved
+haxelib git hscript-improved https://github.com/FNF-CNE-Devs/hscript-improved
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
+haxelib install hxCodec
+haxelib install newgrounds 1.1.4
+haxelib git tentools https://github.com/TentaRJ/tentools
+haxelib git hxcpp https://github.com/yophlox/fxcpp
 haxelib run lime setup -y
 haxelib install flixel-tools
 haxelib install flixel-addons 2.11.0
 haxelib install flixel-ui
-title Lantern Framework! Setup - Press anything to activate lime & flixel tools.
+title Moon4K! Setup - Press anything to activate lime & flixel tools.
 cls
 echo Setting up lime...
 haxelib run lime setup
