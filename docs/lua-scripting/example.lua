4ce9e6995231990087a89bca4c43ecc769ffacb0
Phlox <168133165+yophlox@users.noreply.github.com>
2024-11-28 17:01:23 -0500

    docs shit

---
+++ docs/lua-scripting/example.lua
@@ -0,0 +1,25 @@
+-- example.lua
+function onCreate()
+    -- Called when the script is loaded
+end
+
+
+function onUpdate(elapsed)
+    -- Called every frame
+end
+
+function onNoteHit(noteMs, judgment)
+    -- Called when a note is hit
+end 
+
+function onNoteMiss(direction)
+    -- Called when a note is missed
+end
+
+function onStep()
+    -- Called every step
+end
+
+function onBeat()
+    -- Called every beat
+end
