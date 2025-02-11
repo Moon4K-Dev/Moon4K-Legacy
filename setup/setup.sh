37dc7fb938c5e199631ec3eec6f37982d43ed60f
p <168133165+yophlox@users.noreply.github.com>
2025-02-10 23:31:57 -0500

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,7 @@
 echo Installing needed libs for Moon4K!
 haxelib --global install hmm
 haxelib --global run hmm setup
-hmm install
+cd .. && hmm install
 echo Setting up lime..
 haxelib run lime setup -y
 echo Moon4K! fully set up for building!
